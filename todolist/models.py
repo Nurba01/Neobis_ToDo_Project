d4204dead3e5356e561e3d6b2a6d1fd5adbace92
Nurbolat Mustafin <m.nurbolat01@gmail.com>
2024-03-11 19:26:14 +0500

    Neobis_ToDo_Project

---
+++ todolist/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField('The name of the task', max_length = 500)
+    is_complete = models.BooleanField('Completed', default = False)
+
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return self.title
+
+    
